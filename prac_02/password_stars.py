4ce47724d36671e0cf412480e46266c520e5811a
jd162042 <joel.offerman@my.jcu.edu.au>
2026-09-23 11:35:02 +1000

    Refactor password reader using functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,29 @@
 """Password Reader"""
 
-min_password_length = 7
+# imports
+# CONSTANTS
 
-print(f"Minimum password length is {str(min_password_length)} characters")
+def main():
+    min_password_length = 7
 
-user_password = input("Enter password: ")
-while len(user_password) <= min_password_length:
-    print("Error - set longer password")
+    print(f"Minimum password length is {min_password_length} characters")
+
+    user_password = get_password(min_password_length)
+
+    print_password(user_password)
+
+
+def print_password(user_password: str):
+    print(f"Your password is: {len(user_password) * "*"}")
+
+
+def get_password(min_password_length: int) -> str:
     user_password = input("Enter password: ")
+    while len(user_password) <= min_password_length:
+        print("Error - set longer password")
+        user_password = input("Enter password: ")
+    return user_password
 
-print(f"Your password is: {len(user_password) * "*"}")
+
+main()
+
